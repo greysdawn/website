5724cac7a88875a8ed19d6adb720de795301b676
xgreyskiesx <greysdawn@gmail.com>
2019-10-14 01:54:30 -0400

    Fix exports

--- index.js
+++ index.js
@@ -830,6 +830,6 @@ app.use(function (err, req, res, next) {
   next(err)
 })
 
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
 console.log("Ready.");
-// module.exports = app;
+module.exports = app;
